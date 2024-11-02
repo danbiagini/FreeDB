f9de55b032c86679e0c7a78378d157e9e4c0634d
Dan Biagini <28396789+danbiagini@users.noreply.github.com>
2024-11-02 14:34:29 +0000

    adding traefik systemd setup

--- scripts/traefik-instance.sh
+++ scripts/traefik-instance.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Get the directory of the currently running script
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Construct the full path to the traefik.toml file
+CONFIG_DIR="${SCRIPT_DIR}/../config"
+TRAEFIK_CONFIG_PATH="${CONFIG_DIR}/traefik.toml"
+
 sudo -u incus incus launch images:debian/12/cloud proxy1
 sudo -u incus incus exec proxy1 -- apt update
 sudo -u incus incus exec proxy1 -- apt install -yq git curl
@@ -6,21 +15,36 @@ sudo -u incus incus exec proxy1 -- sudo adduser --system --shell /bin/bash --hom
 sudo -u incus incus exec proxy1 -- sudo usermod -aG sudo traefik
 
 # setup the traefik user with bash and PATH after the gcloud install
-sudo -u incus incus exec proxy1 -- sudo -u traefik cp /etc/skel/.* ~/
+sudo -u incus incus exec proxy1 -- sudo -u traefik cp /etc/skel/.* /home/traefik/
 
-#sudo -u incus incus exec proxy1 -- sudo -u -i git clone https://github.com/danbiagini/FreeDB.git
 
 # check version of traefik
-#incus exec proxy1 -- sudo -u traefik -i curl -L 'https://github.com/traefik/traefik/releases/download/v3.1.6/traefik_v3.1.6_linux_amd64.tar.gz' > traefik_v3.1.6.tar.gz
-#incus exec proxy1 -- sudo -u traefik -i tar -xzvf traefik_v3.1.6.tar.gz 
+echo "Installing traefik hard coded version 3.1.7, should check for updated versions at 'https://github.com/traefik/traefik/releases'"
+sudo -u incus incus exec proxy1 -- sudo -u traefik -i sh -c "curl -L 'https://github.com/traefik/traefik/releases/download/v3.1.7/traefik_v3.1.7_linux_amd64.tar.gz' > traefik_v3.1.7.tar.gz"
+sudo -u incus incus exec proxy1 -- sudo -u traefik -i tar -xzvf traefik_v3.1.7.tar.gz 
 
-#incus exec proxy1 -- sudo -u traefik -i traefik --configFile=FreeDB/config/traefik.toml
+sudo -u incus incus exec proxy1 -- sudo cp /home/traefik/traefik /usr/local/bin/
+sudo -u incus incus exec proxy1 -- sudo chown root:root /usr/local/bin/traefik
+sudo -u incus incus exec proxy1 -- sudo chmod 755 /usr/local/bin/traefik
+sudo -u incus incus exec proxy1 -- sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/traefik
 
-sudo -u incusincus network forward port add incusbr0 10.0.1.14 tcp 80,443,8080 <proxy1-ip>
+sudo -u incus incus exec proxy1 -- sudo mkdir /etc/traefik
+sudo -u incus incus exec proxy1 -- sudo mkdir /etc/traefik/acme
+sudo -u incus incus exec proxy1 -- sudo chown -R root:root /etc/traefik
+sudo -u incus incus exec proxy1 -- sudo chown -R traefik:traefik /etc/traefik/acme
 
-# https://cloud.google.com/iap/docs/using-tcp-forwarding#create-firewall-rule
-# to connect from G CloudShell 
+sudo -u incus incus file push  "$TRAEFIK_CONFIG_PATH" proxy1/etc/traefik/
 
-# to connect to dash
-# gcloud compute start-iap-tunnel freedb 8080 --local-host-port=localhost:8080
-# 'preview on port 8080' in cloud shell 
+sudo -u incus incus exec proxy1 -- sudo chown root:root /etc/traefik/traefik.toml
+sudo -u incus incus exec proxy1 -- sudo chmod 644 /etc/traefik/traefik.toml
+
+sudo -u incus incus file push "${CONFIG_DIR}/traefik.service" /etc/systemd/system/
+
+sudo -u incus incus exec proxy1 -- sudo chown root:root /etc/systemd/system/traefik.service
+sudo -u incus incus exec proxy1 -- sudo chmod 644 /etc/systemd/system/traefik.service
+sudo -u incus incus exec proxy1 -- sudo systemctl daemon-reload
+sudo -u incus incus exec proxy1 -- sudo systemctl start traefik.service
+
+#sudo -u incus incus network forward port add incusbr0 10.0.1.14 tcp 80,443,8080 <proxy1-ip>
+
+#sudo -u incus incus exec proxy1 -- sudo -u traefik -i traefik --configFile=FreeDB/config/traefik.toml
