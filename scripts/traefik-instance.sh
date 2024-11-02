c529dce88d2f833a9da45233c639cfabbfb45d61
Dan Biagini <28396789+danbiagini@users.noreply.github.com>
2024-11-02 03:07:27 +0000

    copy bash dot files from /etc/skil

--- scripts/traefik-instance.sh
+++ scripts/traefik-instance.sh
@@ -1,16 +1,14 @@
-incus launch images:debian/12/cloud proxy1 --profile v1
-incus exec proxy1 -- apt update
-incus exec proxy1 -- apt install -yq git curl
+sudo -u incus incus launch images:debian/12/cloud proxy1
+sudo -u incus incus exec proxy1 -- apt update
+sudo -u incus incus exec proxy1 -- apt install -yq git curl
 
-incus exec proxy1 -- sudo adduser --system --shell /bin/bash --home /home/traefik traefik
-incus exec proxy1 -- sudo usermod -aG sudo traefik
-# setup the postgres user with bash and PATH after the gcloud install
-incus exec proxy1 -- cp .bashrc /home/traefik/
-incus exec proxy1 -- cp .profile /home/traefik/
-incus exec proxy1 -- chown traefik /home/traefik/.bashrc 
-incus exec proxy1 -- chown traefik /home/traefik/.profile
+sudo -u incus incus exec proxy1 -- sudo adduser --system --shell /bin/bash --home /home/traefik traefik
+sudo -u incus incus exec proxy1 -- sudo usermod -aG sudo traefik
 
-incus exec proxy1 -- sudo -u -i git clone https://github.com/danbiagini/FreeDB.git
+# setup the traefik user with bash and PATH after the gcloud install
+sudo -u incus incus exec proxy1 -- sudo -u traefik cp /etc/skel/.* ~/
+
+#sudo -u incus incus exec proxy1 -- sudo -u -i git clone https://github.com/danbiagini/FreeDB.git
 
 # check version of traefik
 #incus exec proxy1 -- sudo -u traefik -i curl -L 'https://github.com/traefik/traefik/releases/download/v3.1.6/traefik_v3.1.6_linux_amd64.tar.gz' > traefik_v3.1.6.tar.gz
@@ -18,7 +16,7 @@ incus exec proxy1 -- sudo -u -i git clone https://github.com/danbiagini/FreeDB.g
 
 #incus exec proxy1 -- sudo -u traefik -i traefik --configFile=FreeDB/config/traefik.toml
 
-incus network forward port add incusbr0 10.0.1.5 tcp 80,443,8080 10.233.59.32
+sudo -u incusincus network forward port add incusbr0 10.0.1.14 tcp 80,443,8080 <proxy1-ip>
 
 # https://cloud.google.com/iap/docs/using-tcp-forwarding#create-firewall-rule
 # to connect from G CloudShell 
